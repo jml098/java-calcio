6cdaa62cb264a76e9cd55a1c7e74bcc5430f6851
jml098 <jmlrissac@gmail.com>
2023-08-02 16:23:00 +0200

    Added goal chance based on strategy

--- src/Partita.java
+++ src/Partita.java
@@ -35,23 +35,34 @@ public class Partita {
         Random random = new Random();
         for (int i = 0; i < 90; i++) {
 
-            if (random.nextInt(100) < 10) {
-                Giocatore giocatoreCheHaSegnato;
+            Squadra squadraChePotrebbeSegnare =
+                    random.nextBoolean() ? squadraDiCasa : squadraOspite;
 
-                if (random.nextInt(2) == 1) {
-                    List<Giocatore> giocatoriSquadraDiCasa = getSquadraDiCasa().getGiocatori();
-                    giocatoreCheHaSegnato = giocatoriSquadraDiCasa.get(random.nextInt(giocatoriSquadraDiCasa.size()));
-                } else {
-                    List<Giocatore> giocatoriSquadraOspite = getSquadraOspite().getGiocatori();
-                    giocatoreCheHaSegnato = giocatoriSquadraOspite.get(random.nextInt(giocatoriSquadraOspite.size()));
-                }
-
+            if (random.nextInt(100) < getProbabilitaDiGoal(squadraChePotrebbeSegnare)) {
+                Giocatore giocatoreCheHaSegnato =
+                        squadraChePotrebbeSegnare.getGiocatori().get(random.nextInt(11));
 
                 goal(giocatoreCheHaSegnato, i);
             }
         }
 
         finisci();
+    }
+
+    private int getProbabilitaDiGoal(Squadra squadra) {
+        if (squadra == squadraDiCasa) {
+            int attaccantiCasa = Integer.parseInt(squadraDiCasa.getAllenatore().getStrategia().split("-")[3]);
+            int difensoriOspite = Integer.parseInt(squadraOspite.getAllenatore().getStrategia().split("-")[1]);
+
+
+            return 10 / ((difensoriOspite + attaccantiCasa) / attaccantiCasa);
+
+        } else {
+            int attaccantiOspite = Integer.parseInt(squadraOspite.getAllenatore().getStrategia().split("-")[3]);
+            int difensoriCasa = Integer.parseInt(squadraDiCasa.getAllenatore().getStrategia().split("-")[1]);
+
+            return 10 / ((difensoriCasa + attaccantiOspite) / attaccantiOspite);
+        }
     }
 
     public void finisci() {
@@ -74,21 +85,21 @@ public class Partita {
         String string = "Partita " + squadraDiCasa.getNome() + " - " + squadraOspite.getNome() + "\n" +
                 "Risultato: " + getRisultato() + "\n";
 
-        String marcatoriCasa = squadraDiCasa.getNome() + ":\n";
-        String marcatoriOspite = squadraOspite.getNome() + ":\n";
+        StringBuilder marcatoriCasa = new StringBuilder(squadraDiCasa.getNome() + ":\n");
+        StringBuilder marcatoriOspite = new StringBuilder(squadraOspite.getNome() + ":\n");
 
 
         for (Goal goal :
                 goalSquadraDiCasa) {
-            marcatoriCasa += goal.getMarcatore().getNome() + " - Minuto: " + goal.getMinuto() + "\n";
+            marcatoriCasa.append(goal.getMarcatore().getNome()).append(" - Minuto: ").append(goal.getMinuto()).append("\n");
         }
 
         for (Goal goal :
                 goalSquadraOspite) {
-            marcatoriOspite += goal.getMarcatore().getNome() + " - Minuto: " + goal.getMinuto()+ "\n";
+            marcatoriOspite.append(goal.getMarcatore().getNome()).append(" - Minuto: ").append(goal.getMinuto()).append("\n");
         }
 
-        string += marcatoriCasa + marcatoriOspite;
+        string += marcatoriCasa + marcatoriOspite.toString();
         return string;
 
     }
